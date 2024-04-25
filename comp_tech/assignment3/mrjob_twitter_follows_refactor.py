d05b5d2735832f26efc358191e323e5216282b87
DJB1ra <145331655+DJB1ra@users.noreply.github.com>
2024-04-25 11:13:07 +0200

    Create mrjob_twitter_follows_refactor.py
    Refactor of Problem 1

---
+++ comp_tech/assignment3/mrjob_twitter_follows_refactor.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+class MRJobTwitterFollows(MRJob):
+
+    def mapper(self, _, line):
+        # Split the line at the colon
+        parts = line.strip().split(':')
+        id_ = parts[0]
+        follows_acc = parts[1].split()  # Split by space, assuming IDs are separated by space
+        # If there are two parts (ID and whatever follows), yield the ID and count of follows
+        if len(parts) == 2:
+            yield (None, (id_, len(follows_acc)))
+
+    def reducer(self, _, followcounts):
+
+        max_follows_count = (None, -1)
+        total_follows = 0
+        unique_ids_count = 0  # determine count of unique ids
+        no_follows_count = 0  # Counter for IDs with no followers
+        for id_, count in followcounts:
+            total_follows += count
+            if count == 0:
+                no_follows_count += 1
+            if count > max_follows_count[1]:            
+                max_follows_count = (id_, count)
+            unique_ids_count += 1
+        yield ('max', max_follows_count)
+        yield ('Follows no account', no_follows_count)  # Yield count of IDs with no followers
+        yield ('avg', total_follows / unique_ids_count)
+
+
+    def steps(self):
+        return [MRStep(mapper=self.mapper),
+                    MRStep(reducer=self.reducer)]
+
+if __name__ == '__main__':
+    MRJobTwitterFollows.run()
