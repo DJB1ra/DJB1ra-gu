1b00d8ea95cf5688697bb6792b4751d444a6909b
guswolfal <145332435+guswolfal@users.noreply.github.com>
2024-04-25 18:01:56 +0200

    my_solution2.mp3
    This time without errors

--- comp_tech/assignment3/mrjob_twitter_followers_Alex.py
+++ comp_tech/assignment3/mrjob_twitter_followers_Alex.py
@@ -8,18 +8,16 @@ class MRJobTwitterFollowers(MRJob):
     def mapper(self, key, value):
         value = value.split(":")
         # if no followers then return ID with 0
-        if len(value[1].split()) < 1:
-            yield(value[0], 0)
+        yield(value[0], 0)
         for f in value[1].split():
             yield(f, 1)
 
-    
+
     def reducer(self, c, counts):
         yield(None ,(c,sum(counts)))
-
+    
     def combiner(self, c, ones):
         yield(c, sum(ones))
-
 
     def reducer2(self, _, fCounts):
         minimumf = (None, float('inf'))
@@ -47,7 +45,7 @@ class MRJobTwitterFollowers(MRJob):
 
     def steps(self):
         return [MRStep(mapper=self.mapper, combiner = self.combiner, reducer=self.reducer), MRStep(reducer=self.reducer2)]
-
+    
 if __name__ == '__main__':
     MRJobTwitterFollowers.run()
 
