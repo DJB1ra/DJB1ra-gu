f7c8e1b1099d1a50d852488746e7d33f3fb07613
guswolfal <145332435+guswolfal@users.noreply.github.com>
2024-05-14 09:44:10 +0200

    Add files via upload

---
+++ comp_tech/ass5/lsh_matmul.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import numpy.typing as npt
+import pandas as pd
+import csv
+import argparse
+import time
+from typing import Dict, Tuple, List
+
+def load_glove(filename: str)->Tuple[Dict[str,int],Dict[int,str],
+                                        npt.NDArray[np.float64]]:
+    """
+    Loads the glove dataset. Returns three things:
+    A dictionary that contains a map from words to rows in the dataset.
+    A reverse dictionary that maps rows to words.
+    The embeddings dataset as a NumPy array.
+    """
+    df = pd.read_table(filename, sep=' ', index_col=0, header=None,
+                           quoting=csv.QUOTE_NONE)
+    word_to_idx: Dict[str,int] = dict()
+    idx_to_word: Dict[int,str] = dict()
+    for (i,word) in enumerate(df.index):
+        word_to_idx[word] = i
+        idx_to_word[i] = word
+    return (word_to_idx, idx_to_word, df.to_numpy())
+
+def normalize(X: npt.NDArray[np.float64])->npt.NDArray[np.float64]:
+    
+    norms = np.linalg.norm(X, axis=1, keepdims=True)
+
+    normalized_matrix = X / norms
+    
+    return normalized_matrix
+
+def construct_queries(queries_fn: str, word_to_idx: Dict[str,int],
+                          X: npt.NDArray[np.float64]) -> \
+                          Tuple[npt.NDArray[np.float64],List[str]]:
+    """
+    Reads queries (one string per line) and returns:
+    - The query vectors as a matrix Q (one query per row)
+    - Query labels as a list of strings
+    """
+    with open(queries_fn, 'r') as f:
+        queries = f.read().splitlines()
+    Q = np.zeros((len(queries), X.shape[1]))
+    for i in range(len(queries)):
+        Q[i,:] = X[word_to_idx[queries[i]],:]
+    return (Q,queries)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dataset', help='Glove dataset filename',
+                            type=str)
+    parser.add_argument('queries', help='Queries filename', type=str)
+    args = parser.parse_args()
+    
+    (word_to_idx, idx_to_word, X) = load_glove(args.dataset)
+
+
+    X = normalize(X)
+
+    (Q,queries) = construct_queries(args.queries, word_to_idx, X)
+
+    Q = normalize(Q)
+
+    t1 = time.time()
+
+    dot_product = np.dot(Q[0], X.T)
+
+    
+    t2 = time.time()
+
+
+    # Compute here I such that I[i,:] contains the indices of the nearest
+    # neighbors of the word i in ascending order.
+    # Naturally, I[i,-1] should then be the index of the word itself.
+    # Rank data vectors based on similarity scores for each query word
+
+    I = np.argsort(dot_product)
+    I = I[::-1]
+    
+
+    
+    counter = 0
+    for i in I:
+        if counter == 0:
+            counter += 1
+            continue
+        if counter == 4:
+            break
+        counter += 1
+        print(idx_to_word[i])
+
+    print(I)
+    print(word_to_idx['halfpipes'])
+    print(word_to_idx['closeouts'])
+    print(word_to_idx['stackers'])
+    print(idx_to_word[7425])
+
+    t3 = time.time()
+    raise NotImplementedError()
+    
+    for i in range(I.shape[0]):
+        neighbors = [idx_to_word[i] for i in I[i,-2:-5:-1]]
+        print(f'{queries[i]}: {" ".join(neighbors)}')
+
+    print('matrix multiplication took', t2-t1)
+    print('sorting took', t3-t2)
+    print('total time', t3-t1)
